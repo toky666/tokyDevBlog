19c955d5471b5448b88f52480e2e3043b42032ff
TokyDev <yokycoronelchavez@gmail.com>
2026-01-06 23:47:25 -0400

    Iniciando fondo de pantalla

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,39 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import solidJs from '@astrojs/solid-js';
+
+import sitemap from '@astrojs/sitemap';
+
+import mdx from '@astrojs/mdx';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    inlineStylesheets: "never",
+  },
+
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      theme: "synthwave-84",
+      defaultColor: false,
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
+
+  site: "https://itssofi.dev/",
+  integrations: [solidJs(), sitemap(), mdx()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
