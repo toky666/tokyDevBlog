ac33329e034f7d1124ca85b6307a9b5678c023ed
TokyDev <yokycoronelchavez@gmail.com>
2026-01-14 23:59:14 -0400

    añadiendo cositas

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'TokyDev';
-export const SITE_DESCRIPTION = 'Web Developer. JavaScript Specialist';
+export const SITE_DESCRIPTION = 'Web Developer. JavaScript Specialist in audit';
